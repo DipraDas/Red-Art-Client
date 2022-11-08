082d4de61933f3688dabe83a9021cd6fdbd0797a
Dipra Das <dipradas5940@gmail.com>
2022-11-09 01:47:54 +0600

    Lets Know

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
     return (
         <div className='background'>
             <div className='container px-5'>
-                <h1 className='text-white font-semibold text-center text-7xl'>STORE YOUR MEMORIES IN CANVAS</h1>
+                <h1 className='text-white font-semibold text-center text-7xl'>KEEP YOUR MEMORIES IN CANVAS</h1>
                 <p className='text-center text-xl text-white tracking-wider mt-8'>Memories are unforgettable, remaining ever vivid and heartwarming!</p>
             </div>
         </div>
--- src/Pages/Home/LetsKnow/LetsKnow.js
+++ src/Pages/Home/LetsKnow/LetsKnow.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import bar from '../../../assets/home/bar-line.png';
 import img1 from '../../../assets/home/blog-image.jpg';
+import img2 from '../../../assets/home/en.jpg';
+import img3 from '../../../assets/home/oii.jpg';
+import img4 from '../../../assets/home/im.jpg';
 
 const LetsKnow = () => {
     return (
@@ -16,6 +19,33 @@ const LetsKnow = () => {
                 <div className='p-10'>
                     <h3 className='text-white font-semibold text-5xl mb-6'>ACRYLIC</h3>
                     <p className='text-gray-400 font-light tracking-wider leading-8'>Acrylic painting, technique in which pigments are mixed with hot, liquid wax. After all of the colours have been applied to the painting surface, a heating element is passed over them until the individual brush or spatula marks fuse into a uniform film.</p>
+                </div>
+            </div>
+            <div className="grid md:grid-cols-2 sm:grid-cols-1 justify-center items-center text-center">
+                <div className='p-10'>
+                    <h3 className='text-white font-semibold text-5xl mb-6'>ENCAUSTIC</h3>
+                    <p className='text-gray-400 font-light tracking-wider leading-8'>Encaustic painting, technique in which pigments are mixed with hot, liquid wax. After all of the colours have been applied to the painting surface, a heating element is passed over them until the individual brush or spatula marks fuse into a uniform film.</p>
+                </div>
+                <div>
+                    <img src={img2} alt="" />
+                </div>
+            </div>
+            <div className="grid md:grid-cols-2 sm:grid-cols-1 justify-center items-center text-center">
+                <div>
+                    <img src={img3} alt="" />
+                </div>
+                <div className='p-10'>
+                    <h3 className='text-white font-semibold text-5xl mb-6'>OIL PAINTING</h3>
+                    <p className='text-gray-400 font-light tracking-wider leading-8'>Oil painting, technique in which pigments are mixed with hot, liquid wax. After all of the colours have been applied to the painting surface, a heating element is passed over them until the individual brush or spatula marks fuse into a uniform film.</p>
+                </div>
+            </div>
+            <div className="grid md:grid-cols-2 sm:grid-cols-1 justify-center items-center text-center">
+                <div className='p-10'>
+                    <h3 className='text-white font-semibold text-5xl mb-6'>IMPASTO</h3>
+                    <p className='text-gray-400 font-light tracking-wider leading-8'>Impasto painting, technique in which pigments are mixed with hot, liquid wax. After all of the colours have been applied to the painting surface, a heating element is passed over them until the individual brush or spatula marks fuse into a uniform film.</p>
+                </div>
+                <div>
+                    <img src={img4} alt="" />
                 </div>
             </div>
         </div>
