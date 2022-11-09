12255bc3575ab356d05b3bbb2bf3a462a4f5d563
Dipra Das <dipradas5940@gmail.com>
2022-11-10 00:54:54 +0600

    Add Service

--- src/Pages/AddService/AddService.js
+++ src/Pages/AddService/AddService.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import bar from '../../../src/assets/home/bar-line.png';
 
 const AddService = () => {
     const handleAddService = event => {
@@ -12,17 +13,17 @@ const AddService = () => {
         const weight = form.weight.value;
         const design = form.design.value;
         const img = form.img.value;
-        
+
         const service = {
 
             name,
-                price,
+            price,
             rating,
-                description,
+            description,
             flavours,
-                weight,
+            weight,
             design,
-                img,
+            img,
         }
 
 
@@ -45,17 +46,29 @@ const AddService = () => {
     }
     return (
         <div>
-            <form onSubmit={handleAddService}>
-                <input type="text" name="name" id="" placeholder='name'/>
-                <input type="text" name="price" id=""  placeholder='price'/>
-                <input type="text" name="rating" id=""  placeholder='rating'/>
-                <input type="text" name="description" id=""  placeholder='description'/>
-                <input type="text" name="flavours" id=""  placeholder='flavours'/>
-                <input type="text" name="weight" id="" placeholder='weight' />
-                <input type="text" name="design" id=""  placeholder='design'/>
-                <input type="text" name="img" id=""  placeholder='img'/>
-                <input className='btn' type="submit" value="Place Your Order" />
-            </form>
+            <div className="container mx-auto px-10 py-10 text-center">
+                <h2 className='text-6xl mb-10 font-semibold text-gray-100'><span style={{ color: 'red' }}>Add</span> Services</h2>
+                <img className='m-auto' src={bar} alt="" />
+            </div>
+            <div className="w-2/4 mx-auto pb-20 pt-10">
+                <form onSubmit={handleAddService}>
+                    <label className='mt-3' htmlFor="">Name</label> <br />
+                    <input name='name' type="text" placeholder="Add Service Name" className="input input-bordered w-full mb-3" required /><br />
+                    <label className='mt-4' htmlFor="">Price</label><br />
+                    <input name='price' type="text" placeholder="Add Price" className="input input-bordered w-full mb-3" required /><br />
+                    <label className='mt-4' htmlFor="">Rating</label><br />
+                    <input name='rating' type="text" placeholder="Enter Service Rating" className="input input-bordered w-full mb-3" required /><br />
+                    <label className='mt-4' htmlFor="">Design</label><br />
+                    <input name='design' type="text" placeholder="Enter Design Abailability" className="input input-bordered w-full mb-3" required /><br />
+                    <label className='mt-4' htmlFor="">Image</label><br />
+                    <input name='image' type="text" placeholder="Enter Image URL" className="input input-bordered w-full mb-3" required /><br />
+                    <label className='mt-4' htmlFor="">Description</label><br />
+                    <input name='description' type="text" placeholder="Enter Service Description" className="input input-bordered w-full " required /><br />
+                    <div className="card-actions">
+                        <button style={{ border: '1px solid #ff6868', backgroundColor: '#000', textTransform: 'uppercase', boxShadow: '0px 0px 5px 0px inset #ff6868' }} className="btn btn-block mt-5">ADD SERVICE</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
--- src/Pages/Services/Services.js
+++ src/Pages/Services/Services.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
 import ServiceCard from '../Home/ServiceHome/ServiceCard';
+import bar from '../../../src/assets/home/bar-line.png';
 
 const Services = () => {
     const [services, setServices] = useState([]);
@@ -18,7 +18,10 @@ const Services = () => {
     }, [size])
     return (
         <div>
-            <h1>Services</h1>
+            <div className="container mx-auto px-10 py-10 text-center">
+                <h2 className='text-6xl mb-3 font-semibold text-gray-100'><span style={{ color: 'red' }}>Serv</span>ices</h2>
+                <img className='m-auto' src={bar} alt="" />
+            </div>
             <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
                 {
                     services.map(service => <ServiceCard
