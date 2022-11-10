f8c593a9ab91f7e080c91ce308fb24d0f31edd2f
Dipra Das <dipradas5940@gmail.com>
2022-11-10 15:04:13 +0600

    my reviews

--- src/Pages/MyReviews/MyReviews.js
+++ src/Pages/MyReviews/MyReviews.js
@@ -1,28 +1,28 @@
-// import React, { useContext, useEffect, useState } from 'react';
-// import { AuthContext } from '../../context/AuthProvider/AuthProvider';
-// import ReviewCard from '../Services/ReviewSection/ReviewCard';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../context/AuthProvider/AuthProvider';
+import ReviewCard from '../Services/ReviewSection/ReviewCard';
 
-// const MyReviews = () => {
-//     const { user } = useContext(AuthContext);
-//     const [reviews,setReviews]=useState([])
+const MyReviews = () => {
+    const { user } = useContext(AuthContext);
+    const [reviews,setReviews]=useState([])
 
-//     useEffect(() => {
-//         fetch(`http://localhost:5000/myReviews?email=${user?.email}`)
-//             .then(res => res.json())
-//             .then(data => setReviews(data))
-//     }, [user?.email])
-//     return (
-//         <div>
-//             {
-//                 reviews.map(review =>
-//                     <ReviewCard
-//                         key={review._id}
-//                         review={review}
-//                     ></ReviewCard>
-//                 )
-//             }
-//         </div>
-//     );
-// };
+    useEffect(() => {
+        fetch(`http://localhost:5000/myReviews?email=${user?.email}`)
+            .then(res => res.json())
+            .then(data => setReviews(data))
+    }, [user?.email])
+    return (
+        <div>
+            {
+                reviews.map(review =>
+                    <ReviewCard
+                        key={review._id}
+                        review={review}
+                    ></ReviewCard>
+                )
+            }
+        </div>
+    );
+};
 
-// export default MyReviews;
+export default MyReviews;
